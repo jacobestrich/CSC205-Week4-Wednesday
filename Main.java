bd3e83b72b57d93a959dbbe4824c4bc19b994bae
jacob.estrich <jacob.s.estrich@gmail.com>
2023-11-15 14:21:41 -0800

    Edit - Made Main

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(new Dog().GetSound());
+        System.out.println(new Cat().GetSound());
+        System.out.println(new Cow().GetSound());
+
+    }
+}
